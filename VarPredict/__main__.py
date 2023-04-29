92a6dc17a9267dca252ec8eec6751163df71c708
Adam Dinan <adam1989ie@gmail.com>
2023-04-29 12:51:49 +0100

    Import parser for EN model in main

--- VarPredict/__main__.py
+++ VarPredict/__main__.py
@@ -1,10 +1,8 @@
 import os, sys
 import argparse
-
 from .__init__ import __version__
-
 from .rf_model import rf_parser
-# from .en_model import en_parser
+from .en_model import en_parser
 
 class UltimateHelpFormatter(
     argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
