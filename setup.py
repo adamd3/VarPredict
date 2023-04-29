ea2b6495165abad990d47f1cc2b368af3f3c15f0
Adam Dinan <adam1989ie@gmail.com>
2023-04-29 14:03:03 +0100

    Fix import for setup script

--- setup.py
+++ setup.py
@@ -3,28 +3,28 @@ import re
 import io
 import os
 
-
 # Get version 
 def read(*names, **kwargs):
     with io.open(os.path.join(os.path.dirname(__file__), *names),
-                 encoding=kwargs.get("encoding", "utf8")) as fp:
+            encoding=kwargs.get('encoding', 'utf8')) as fp:
         return fp.read()
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r'^__version__ = ['\']([^'\']*)['\']',
+        version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    raise RuntimeError('Unable to find version string.')
 
-setuptools.setup(
-    name="VarPredict",
-    version=find_version("VarPredict/__init__.py"),
-    author="Adam Dinan",
-    author_email="ad866@cam.ac.uk",
-    description="A command line tool for modelling gene expression using genetic variants and covariates",
-    url="https://github.com/adamd3/VarPredict",
+setup(
+    name='VarPredict',
+    version=find_version('VarPredict/__init__.py'),
+    author='Adam Dinan',
+    author_email='ad866@cam.ac.uk',
+    description='A command line tool for modelling gene expression using '+ 
+        'genetic variants and covariates',
+    url='https://github.com/adamd3/VarPredict',
     packages=['VarPredict'],
     python_requires='>=3.7.0',
     install_requires=['matplotlib', 'numpy', 'pandas', 'scikit-learn'],
@@ -40,5 +40,5 @@ setuptools.setup(
         'console_scripts':
         ['VarPredict = VarPredict.__main__:main',],
     },
-    extras_require={"test": "pytest"},
+    extras_require={'test': 'pytest'},
 )
