bfd25893bbaaf9be3161e41774469221ffe0cc6f
Adam Dinan <adam1989ie@gmail.com>
2023-04-30 10:33:22 +0100

    Fix data_dir parameterisation

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,6 @@ def pytest_addoption(parser):
 
 
 def pytest_generate_tests(metafunc):
-    if "data_dir" in metafunc.fixturenames:
-        metafunc.parametrize("data_dir",
-            metafunc.config.getoption("data_dir"))
+    option_value = metafunc.config.getoption("data_dir")
+    if "data_dir" in metafunc.fixturenames and option_value is not None:
+        metafunc.parametrize("data_dir", [option_value])
